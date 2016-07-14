b89dbf15f2414926e19cbf889950bbfd1222ae23
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-07-14 10:20:35 -0700

    Allow notes to be blank

--- to_do/tasks/models.py
+++ to_do/tasks/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
 
     task = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    notes = models.CharField(max_length=200)
+    notes = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         """Return string."""
