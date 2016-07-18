e788c61164a32e642636b734ec543c4d94fe2901
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-07-18 12:08:53 -0700

    Add Task viewset

--- to_do/tasks_api/views.py
+++ to_do/tasks_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from tasks.models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """Task viewset."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer()
